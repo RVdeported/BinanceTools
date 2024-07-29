20105c0e8d81b4f4a568c3fd94a0f475feb804f1
RVdeported <RomanVetrin@gmail.com>
2024-07-29 20:05:43 +0000

    Corrected trades to reflect more accurate timestamp

--- acc_trades.py
+++ acc_trades.py
@@ -10,12 +10,15 @@ if __name__ == "__main__":
         print("Enter symbol!")
         exit()
     
-    res = client.get_account_trades(symbol=sys.argv[1], recWindow=6000)
+    res = client.get_all_orders(symbol=sys.argv[1], recWindow=6000, limit=100)
     res = sorted(res, reverse=True, key=lambda x: x["time"])
 
 
     for n in res:
+        if n["status"] != "FILLED":
+            continue
         t = dt.fromtimestamp(n["time"] / 1000)
-        print(colored("{}:\tPx:{}\tqty:{}\ttime:{}\tmaker:{}".format(
-            n["symbol"], n["price"], n["qty"], t.strftime("%d.%m.%Y, %H:%M:%S"), n["maker"])
-            , "green" if n["buyer"] else "yellow"))
+        print(colored("{}:  Px:{}\tqty:{}\ttime:{}\tReqID:{}".format(
+            n["symbol"], n["avgPrice"], n["executedQty"], t.strftime("%d.%m.%Y, %H:%M:%S.%f"), 
+            n["clientOrderId"])
+            , "green" if n["side"]=="BUY" else "yellow"))
