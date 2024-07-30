45f47fb6c50872f22666768f1723313bab13d86f
RVdeported <romanvetrin@gmail.com>
2024-07-30 09:49:14 +0200

    Minor fixes

--- acc_info.py
+++ acc_info.py
@@ -19,21 +19,22 @@ def get_positions():
     assets_with_pos = [ass for ass in assets if abs(float(ass["positionAmt"])) > 1e-10]
     return assets_with_pos, res
 
-def get_positions_str():
+def get_positions_str(col = False):
     assets, acc_info = get_positions()
     out ="Available Balance: {}\tUPNL:{}\tWithMargin: {}\n".format(
             acc_info["availableBalance"], acc_info["totalCrossUnPnl"], 
             float(acc_info["availableBalance"]) + 
             float(acc_info["totalInitialMargin"]))
     for n in assets:
-        out += colored(
-                "{}:\tentryPx:{}\tAmnt:{}\tUPNL:{:.2f}({:.2f}%)\tNotional:{}\n"\
+        s = "{}:\tentryPx:{}\tAmnt:{}\tUPNL:{:.2f}({:.2f}%)\tNotional:{}\n"\
                 .format(n["symbol"], n["entryPrice"],n["positionAmt"], 
                         float(n["unrealizedProfit"]), 
                         float(n["unrealizedProfit"]) \
                             / abs(float(n["notional"])) * 100.0,
-                        n["notional"]), 
-                "green" if float(n["positionAmt"]) > 0 else "yellow")
+                        n["notional"])        
+        color = "green" if float(n["positionAmt"]) > 0 else "yellow"
+        out += colored(s, color ) if col else s
+
     return out
 
 
@@ -41,6 +42,6 @@ def get_positions_str():
 if __name__ == "__main__":
     freq_upd_sec = int(sys.argv[1]) if len(sys.argv) == 2 else 10
     while(True):
-        print(get_positions_str())        
+        print(get_positions_str(True))        
         time.sleep(freq_upd_sec)
 
